6a2019ad726c98e0993ab5840e32615144d4401c
Pramod <23319210+pramodwaghmare@users.noreply.github.com>
2025-12-16 20:04:32 +0530

    update data imputation

--- data_preprocessing/sample_01_data_imputation.py
+++ data_preprocessing/sample_01_data_imputation.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame({
-    'Age': [25, np.nan, 40, 35],
-    'Salary': [50000, 60000, np.nan, 55000]
+    'Age': [25, np.nan, 40, 35, 45],
+    'Salary': [50000, 60000, np.nan, 55000, 65000]
 })
 
 # Mean imputation
